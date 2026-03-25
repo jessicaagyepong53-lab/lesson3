669fb4314e468312e81927759d3a8297345ac020
Agyepong Jessica <jessicaagyepong53@gmail.com>
2026-03-25 12:56:49 +0000

    learning js

---
+++ src/script.js
@@ -0,0 +1,33 @@
+console.log("Hello, T4G Scholars!");
+
+let username = "Jessica";
+console.log(username); // Jessica
+
+console.log("What is your age?");
+let age = prompt("What is your age?");
+console.log(age);
+
+let num1 = 50;
+let num2 = 150;
+// let sum = num1 + num2;
+// console.log(sum); // 200
+
+console.log(num1 + num2); // 200
+console.log(num2 - num1); // 100
+console.log(num2 / num1); // 3
+console.log(num1 * num2); // 7500
+console.log(15 % 4); // 3 what is left after dividing 15 by 4
+
+let h = true;
+console.log(h); // true
+
+let j = false;
+console.log(j); // false
+
+let k = 20
+typeof k; // number
+console.log(typeof k); // number
+
+let m = ("30")
+typeof m; // string
+console.log(typeof m); // string
